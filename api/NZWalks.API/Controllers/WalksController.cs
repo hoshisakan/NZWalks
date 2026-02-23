c1247323db53b14d175b889c01a35bf5ceee2ed3
奕銓 趙 <hoshisakan@gmail.com>
2026-02-23 14:50:33 +0800

    1. 新增 walk API Filtering、Sorting、Pagination 功能。

--- api/NZWalks.API/Controllers/WalksController.cs
+++ api/NZWalks.API/Controllers/WalksController.cs
@@ -29,9 +29,12 @@ namespace NZWalks.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery,
+        [FromQuery] string? sortBy, [FromQuery] bool isAscending,
+        [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000)
         {
-            var walks = await _walkRepository.GetAllAsync();
+            var walks = await _walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, 
+                isAscending, pageNumber, pageSize);
 
             var walksDto = _mapper.Map<List<WalkDto>>(walks);
 
--- api/NZWalks.API/Repositories/WalkRepository.cs
+++ api/NZWalks.API/Repositories/WalkRepository.cs
@@ -19,20 +19,60 @@ namespace NZWalks.API.Repositories
             _nZWalksDbContext = nZWalksDbContext;
         }
 
-        public async Task<List<Walk>> GetAllAsync()
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+                string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
         {
-            return await _nZWalksDbContext.Walks.Include(x => x.Difficulty).Include(x => x.Region).ToListAsync();
+            var walks = _nZWalksDbContext.Walks.Include(x => x.Difficulty).Include(x => x.Region).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(filterOn) && !string.IsNullOrWhiteSpace(filterQuery))
+            {
+                if (filterOn.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = walks.Where(x => x.Name.Contains(filterQuery));
+                }
+                else if (filterOn.Equals("Description", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = walks.Where(x => x.Description.Contains(filterQuery));
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(sortBy))
+            {
+                if (sortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.Name) : walks.OrderByDescending(x => x.Name);
+                }
+                else if (sortBy.Equals("LengthInKm", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
+                }
+                else if (sortBy.Equals("Region", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.Region.Name) : walks.OrderByDescending(x => x.Region.Name);
+                }
+                else if (sortBy.Equals("Difficulty", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.Difficulty.Name) : walks.OrderByDescending(x => x.Difficulty.Name);
+                }
+            }
+
+            var skipNumber = (pageNumber - 1) * pageSize;
+            walks = walks.Skip(skipNumber).Take(pageSize);
+
+            return await walks.ToListAsync();
         }
 
         public async Task<Walk> GetByIdAsync(Guid id)
         {
             return await _nZWalksDbContext.Walks.Include(x => x.Difficulty).Include(x => x.Region).FirstOrDefaultAsync(x => x.Id == id);
-        }   
+        }
 
         public async Task<Walk> CreateAsync(Walk walk)
         {
             await _nZWalksDbContext.Walks.AddAsync(walk);
+
             await _nZWalksDbContext.SaveChangesAsync();
+
             return walk;
         }
 
